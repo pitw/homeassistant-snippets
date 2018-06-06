962e4e3d43533c6b21308f05771ae6628ccd024c
sp <5676857+pitw@users.noreply.github.com>
2018-06-06 22:54:49 +0200

    Create temperature.py

---
+++ edimax-ic-7113w/temperature.py
@@ -0,0 +1,14 @@
+import requests
+from requests.auth import HTTPDigestAuth
+import xml.etree.ElementTree as ET
+
+url = 'http://***CAM_IP***/camera-cgi/admin/param.cgi?action=list&group=SystemInfo'
+
+response = requests.get(url, auth=HTTPDigestAuth('***CAM_USER***', '***CAM_PASSWORD***'))
+if(response.ok):
+ tree = ET.fromstring(response.content)
+ values = tree.find('envSensor')
+ temp = values.find('Temperature')
+ print(temp.text)
+else:
+ response.raise_for_status()
